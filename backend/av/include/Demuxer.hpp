6e35056098d3d27ecde8369d9528a83ac8ae3df6
EricLipe17 <ericlipe17@gmail.com>
2024-08-04 09:27:52 -0600

    - Creating new classes for the muxer and demuxer - CLang formatting from now on

---
+++ backend/av/include/Demuxer.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
+
+#include <string>
+#include <vector>
+
+class Demuxer {
+private:
+  AVFormatContext *m_pInFmtCtx;
+  std::vector<AVCodecContext *> m_vDecCodecCtxs;
+  std::vector<AVFrame *> m_vDecFrames;
+  AVDictionary *m_pOpts;
+  const char *m_pFn;
+  int m_nErrCode;
+  std::string m_szErrMsg;
+
+  void openInput();
+
+public:
+  Demuxer(const char *_pFn, AVDictionary *_pOpts);
+  ~Demuxer();
+  AVFrame *Frame(AVPacket *_pPacket);
+};
