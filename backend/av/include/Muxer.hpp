5a091c0ee27b412f29e9dac3ce6a02eac7498361
EricLipe17 <ericlipe17@gmail.com>
2024-08-11 16:43:00 -0600

    - Initial stub for muxer class

--- backend/av/include/Muxer.hpp
+++ backend/av/include/Muxer.hpp
@@ -1,1 +1,39 @@
+#pragma once
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavformat/avformat.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
+#include <libavutil/pixdesc.h>
+}
+
+#include "AVUtil.hpp"
+
+struct FilteringContext {
+    AVFilterContext* m_pBufferSinkCtx;
+    AVFilterContext* m_pBufferSrcCtx;
+    AVFilterGraph* m_pFilterGraph;
+
+    AVPacket* enc_pkt;
+    AVFrame* pFilteredFrame;
+};
+
+class Muxer {
+   private:
+    FilteringContext m_filterCtx;
+
+    void openInput();
+    void initFilter();
+    void filterEncodeWriteFrame();
+    void encodeWriteFrame();
+
+   public:
+    Muxer(const char* _pFn, AVDictionary* _pOpts);
+    ~Muxer();
+    void InitFilters();
+    void WriteBuffer(const FrameBuffer& _demuxerBuf);
+    void Flush();
+};
---
+++ backend/av/src/Muxer.cpp
@@ -0,0 +1,19 @@
+#include "Muxer.hpp"
+
+void Muxer::openInput() {}
+
+void Muxer::initFilter() {}
+
+void Muxer::filterEncodeWriteFrame() {}
+
+void Muxer::encodeWriteFrame() {}
+
+Muxer::Muxer(const char* _pFn, AVDictionary* _pOpts) {}
+
+Muxer::~Muxer() {}
+
+void Muxer::InitFilters() {}
+
+void Muxer::WriteBuffer(const FrameBuffer& _demuxerBuf) {}
+
+void Muxer::Flush() {}
