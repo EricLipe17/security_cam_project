c329dfe5f11371e963dc8eee1b02c02fe5c2b8a2
EricLipe17 <ericlipe17@gmail.com>
2024-08-17 12:22:03 -0600

    - Updated cmake to link correct library - Updated build script to build static libs

--- backend/scripts/build_boost.sh
+++ backend/scripts/build_boost.sh
@@ -2,5 +2,5 @@
 
 pushd $BOOST_HOME
 ./bootstrap.sh --prefix=$BOOST_BUILD
-./b2 --build-dir=$BOOST_BUILD --prefix=$BOOST_BUILD
+./b2 --build-dir=$BOOST_BUILD --prefix=$BOOST_BUILD link=static runtime-link=static
 popd
